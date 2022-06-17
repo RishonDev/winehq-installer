b066e5ad7ba7967b0d4912794bb2e25327f7e556
Rishon Jonathan R <mithicalaviator85@gmail.com>
2022-06-17 23:00:45 +0530

    small bug fix

--- get-wine-7.10-ubuntu_22_04.sh
+++ get-wine-7.10-ubuntu_22_04.sh
@@ -1,4 +1,5 @@
 echo "Setting up dependencies..."
+sudo add-apt-repository universe
 sudo dpkg --add-architecture i386
 wget -nc https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
 sudo mv winehq-jammy.sources /etc/apt/sources.list.d/
