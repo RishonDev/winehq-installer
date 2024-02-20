0e477e206e96c4fcfdcc263ac9e643825bc27c9c
Rishon Jonathan R <mithicalaviator85@gmail.com>
2024-02-20 13:43:38 +0530

    Update wine-installer-ubuntu.sh

--- wine-installer-ubuntu.sh
+++ wine-installer-ubuntu.sh
@@ -2,7 +2,7 @@
 while getopts ":v:" opt; do
   case $opt in
     v)
-      echo "2.1.0"
+      echo "3.0.0"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -57,6 +57,11 @@ if [ "$RELEASE" = "22.04" ]; then
     sudo apt update 
 fi
 
+if [ "$RELEASE" = "23.10" ]; then
+    sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/mantic/winehq-mantic.sources
+    sudo apt update 
+fi
+
 if [ "$RELEASE" = "23.04" ]; then
     sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/lunar/winehq-lunar.sources
     sudo apt update 
@@ -68,15 +73,28 @@ fi
     sudo apt update
 } &> ./logs/repoLog.txt
 kill -9 $SPIN_PID
-
-read -p "Install wine? [Y/n]: " -n 1 -r
+clear
+echo "1)Stable build (Recommended)"
+echo "2)Development build (Recommended for testing use only)"
+echo "3)Staging build (Recommended for testing use only)"
+read -p "Select build channel:" build -n 1 -r
+if ["$build" = "1"]
+  build = "stable"
+fi
+if ["$build" = "2"]
+  build = "devel"
+fi
+if ["$build" = "3"]
+  build = "staging"
+fi
+read -p "Ready? [Y/n]: " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
     then
     echo
     spin &
     SPIN_PID=$!
     trap "kill -9 $SPIN_PID" `seq 0 15`
-    sudo apt install winehq-stable winetricks &> ./logs/installLog.txt
+    sudo apt install winehq-"$build" winetricks &> ./logs/installLog.txt
     winecfg &> ./logs/configLog.txt
 else 
     echo
